872999f65c71ca8d5eeb163e23c02f1d68504ffc
Rohit5292 <110280801+Rohit5292@users.noreply.github.com>
2023-04-02 07:54:00 +0530

    full functionality added

--- src/App.js
+++ src/App.js
@@ -1,55 +1,114 @@
-import './App.css';
-import {useState} from 'react'
-
+import { useState } from "react";
+import {
+ evaluate
+} from 'mathjs'
 
 function App() {
-  const[result,setresult]= useState('');
+  const [result, setResult] = useState("");
 
-  function Keypress(event){
-    if (event.target.value==='AC'){
-      setresult('');
+  function handleClick(event) {
+    const value = event.target.value;
+    switch (value) {
+      case "C":
+        setResult("");
+        break;
+      case "+/-":
+        if (result.charAt(0) === "-") {
+          setResult(result.slice(1));
+        } else {
+          setResult("-" + result);
+        }
+        break;
+      case "=":
+        try{
+        setResult(String(evaluate(result)));
+        }
+        catch{
+          setResult("invalid input")
+        }
+        break;
+      case "+":
+      case "-":
+      case "*":
+      case "/":
+        if (
+          result === "" ||
+          result.charAt(result.length - 1) === "+" ||
+          result.charAt(result.length - 1) === "-" ||
+          result.charAt(result.length - 1) === "*" ||
+          result.charAt(result.length - 1) === "/"
+        ) {
+          break;
+        } else {
+          setResult(result.concat(value));
+          break;
+        }
+      default:
+        setResult(result.concat(value));
+        break;
     }
-    else if(event.target.value==="+/-"){
-      if(result.charAt(0)==='-'){
-        setresult((result));
-      }
-      else{
-        setresult('-'+result);
-      }
-    }
-    else if (event.target.value==="="){
-   setresult( String(eval(result))); 
-    }
-    else{
-    setresult(result.concat(event.target.value));
   }
-  }
-  
 
   return (
-    <div className="Body">
-    <div className="Calculator">
-    <input type="text" value={result}/>
-      <button value='1' onClick={Keypress}>1</button>
-      <button value='2'onClick={Keypress}>2</button>
-      <button value='3'onClick={Keypress}>3</button>
-      <button value='4'onClick={Keypress}>4</button>
-      <button value='5'onClick={Keypress}>5</button>
-      <button value='6'onClick={Keypress}>6</button>
-      <button value='7'onClick={Keypress}>7</button>
-      <button value='8'onClick={Keypress}>8</button>
-      <button value='9'onClick={Keypress}>9</button>
-      <button value='0'onClick={Keypress}>0</button>
-      <button value='+'onClick={Keypress}>+</button>
-      <button value='-'onClick={Keypress}>-</button>
-      <button value='*'onClick={Keypress}>*</button>
-      <button value='/'onClick={Keypress}>/</button>
-      <button value='='onClick={Keypress}>=</button>
-      <button value='AC'onClick={Keypress}>AC</button>
-      <button value='+/-'onClick={Keypress}>+/-</button>
-      <button value='%'onClick={Keypress}>%</button>
-      </div>
-      
+    <div className="calculator">
+      <input type="text" value={result} />
+      <button value="C" onClick={handleClick}>
+        C
+      </button>
+      <button value="+/-" onClick={handleClick}>
+        +/-
+      </button>
+      <button value="%" onClick={handleClick}>
+        %
+      </button>
+      <button value="/" onClick={handleClick}>
+        ÷
+      </button>
+      <button value="7" onClick={handleClick}>
+        7
+      </button>
+      <button value="8" onClick={handleClick}>
+        8
+      </button>
+      <button value="9" onClick={handleClick}>
+        9
+      </button>
+      <button value="*" onClick={handleClick}>
+        ×
+      </button>
+      <button value="4" onClick={handleClick}>
+        4
+      </button>
+      <button value="5" onClick={handleClick}>
+        5
+      </button>
+      <button value="6" onClick={handleClick}>
+        6
+      </button>
+      <button value="-" onClick={handleClick}>
+        -
+      </button>
+      <button value="1" onClick={handleClick}>
+        1
+      </button>
+      <button value="2" onClick={handleClick}>
+        2
+      </button>
+      <button value="3" onClick={handleClick}>
+        3
+      </button>
+      <button value="+" onClick={handleClick}>
+        +
+      </button>
+      <button value="0" onClick={handleClick}>
+        0
+      </button>
+      <button value="." onClick={handleClick}>
+        .
+      </button>
+      <button value="=" onClick={handleClick}>
+        =
+      </button>
     </div>
   );
 }
